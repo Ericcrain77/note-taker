95531e8290efdd9c6d16d73b261e23592de902ff
Eric Crain <ericcrain77@gmail.com>
2021-11-02 21:42:33 -0400

    refactoring to get deployed application to function correctly

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const fs = require('fs');
 
-const PORT = process.env.PORT || 3001;
 const app = express();
+const PORT = process.env.PORT || 3001;
+// const app = express();
 
 
 app.use(express.urlencoded({ extended: true }));
