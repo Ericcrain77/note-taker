0808c7388bbfebf01a9bcaa1d65475ca0455ec7a
Eric Crain <ericcrain77@gmail.com>
2021-11-02 22:19:15 -0400

    refactoring

--- server.js
+++ server.js
@@ -10,12 +10,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './Develop/public/notes.html'));
-});
-
 app.get('/api/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './Develop/db/db.json'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './Develop/public/notes.html'));
 });
 
 app.get('*', (req, res) => {
