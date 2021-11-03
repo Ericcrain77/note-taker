ec6a59baed79eb223f842195d012529dd0a437a8
Eric Crain <ericcrain77@gmail.com>
2021-11-02 22:06:40 -0400

    refactoring

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const fs = require('fs');
 
-
-const PORT = process.env.PORT || 3000;
 const app = express();
+const PORT = process.env.PORT || 3001;
 
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('Develop'));
+app.use(express.static('public'));
 
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './Develop/public/notes.html'));
