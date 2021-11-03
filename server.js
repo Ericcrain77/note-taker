4c7d3810290452d2ab06967c0a510919937711d8
Eric Crain <ericcrain77@gmail.com>
2021-11-02 21:36:52 -0400

    refactoring paths

--- server.js
+++ server.js
@@ -10,24 +10,24 @@ app.use(express.json());
 app.use(express.static('public'));
 
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '/Develop/public/notes.html'));
+    res.sendFile(path.join(__dirname, './Develop/public/notes.html'));
 });
 
 app.get('/api/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '/Develop/db/db.json'));
+    res.sendFile(path.join(__dirname, './Develop/db/db.json'));
 });
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/Develop/public/index.html'));
+    res.sendFile(path.join(__dirname, './Develop/public/index.html'));
 });
 
 app.post('/api/notes', (req, res) => {
-    const existingNote = JSON.parse(fs.readFileSync('/Develop/db/db.json'));
+    const existingNote = JSON.parse(fs.readFileSync('./Develop/db/db.json'));
     const newNote = req.body;
     newNote.id = uniqid();
     existingNote.push(newNote);
 
-    fs.writeFileSync('/Developdb/db.json', JSON.stringify(existingNote));
+    fs.writeFileSync('./Developdb/db.json', JSON.stringify(existingNote));
     res.json(existingNote);
 });
 
