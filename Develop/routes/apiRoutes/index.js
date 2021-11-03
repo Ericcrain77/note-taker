33277701ac9f9039d1c21951c95c6f6f1363526c
Eric Crain <ericcrain77@gmail.com>
2021-11-02 21:21:43 -0400

    adding Heroku

---
+++ Develop/routes/apiRoutes/index.js
@@ -0,0 +1,44 @@
+const router = require('express').Router();
+const { notes } = require('../../db/db');
+
+function filterByQuery(query, animalsArray) {
+    let personalityTraitsArray = [];
+    let filteredResults = animalsArray;
+    if (query.personalityTraits) {
+      if (typeof query.personalityTraits === 'string') {
+        personalityTraitsArray = [query.personalityTraits];
+      } else {
+        personalityTraitsArray = query.personalityTraits;
+      }
+      console.log(personalityTraitsArray);
+      personalityTraitsArray.forEach(trait => {
+        filteredResults = filteredResults.filter(
+          animal => animal.personalityTraits.indexOf(trait) !== -1
+        );
+      });
+    }
+    
+    return filteredResults;
+};
+
+function findById(id, notesArray) {
+    const result = notesArray.filter(animal => animal.id === id)[0];
+    return result;
+};
+
+router.get('/notes', (req, res) => {
+    let results = notes;
+    if (req.query) {
+      results = filterByQuery(req.query, results);
+    }
+    res.json(results);
+});
+
+router.get('/api/notes/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+      res.json(result);
+    } else {
+      res.send(404);
+    }
+});
---
+++ Develop/server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const fs = require('fs');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
+
+app.get('/api/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/db/db.json'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+app.post('/api/notes', (req, res) => {
+    const existingNote = JSON.parse(fs.readFileSync('/db/db.json'));
+    const newNote = req.body;
+    newNote.id = uniqid();
+    existingNote.push(newNote);
+
+    fs.writeFileSync('db/db.json', JSON.stringify(existingNote));
+    res.json(existingNote);
+});
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
